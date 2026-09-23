ca4acac913c9fd88981538d89f1d82f6b66475b4
Fizzah Farooq <157381243+FizzahFarooq@users.noreply.github.com>
2026-09-24 01:10:09 +0500

    Update HappyJenkins.java

--- HappyJenkins.java
+++ HappyJenkins.java
@@ -1,6 +1,6 @@
 public class HappyJenkins{
   public static void main(String[] args){
     System.out.print("Hello Everyone");
-    System.out.println("Github Successfull");
+    System.out.println("Github Successful");
 }
 }
