bda308ef80f753217ba84a41cab3409e5098cbcc
Fizzah Farooq <157381243+FizzahFarooq@users.noreply.github.com>
2026-09-23 14:11:35 +0500

    Update greeting message to 'Hello v2'

--- HappyJenkins.java
+++ HappyJenkins.java
@@ -1,6 +1,6 @@
 public class HappyJenkins{
   public static void main(String[] args){
-    System.out.print("Hello");
+    System.out.print("Hello v2");
     System.out.println("Github Successfull");
 }
 }
